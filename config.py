57db0c3abb9ace80cd856af300f6c79dbda542d0
issei711 <i_s3.10@icloud.com>
2025-03-12 13:34:19 +0900

    config.jsonを環境変数に

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 
 # 環境変数を読み込む
@@ -6,5 +7,14 @@ load_dotenv()
 
 class Config:
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    GOOGLE_CREDENTIALS = os.path.join(BASE_DIR, "config.json")  # Google API 認証ファイル
     SPREADSHEET_KEY = os.getenv("SPREADSHEET_KEY")  # 環境変数から取得
+
+    # Google API 認証情報を環境変数から取得
+    GOOGLE_CREDENTIALS_JSON = os.getenv("GOOGLE_CREDENTIALS_JSON")
+
+    if GOOGLE_CREDENTIALS_JSON:
+        GOOGLE_CREDENTIALS = os.path.join(BASE_DIR, "config.json")
+        with open(GOOGLE_CREDENTIALS, "w") as f:
+            json.dump(json.loads(GOOGLE_CREDENTIALS_JSON), f)
+    else:
+        raise ValueError("環境変数 GOOGLE_CREDENTIALS_JSON が設定されていません。")
